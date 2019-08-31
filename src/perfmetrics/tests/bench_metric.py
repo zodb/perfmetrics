e105e7f7b34f969cbc860239173c02664d76cced
Jason Madden <jamadden@gmail.com>
2019-08-31 07:03:13 -0500

    More tests of sampling.
    CPy27, CPy37, PyPy27:
    
    +---------------------------------------------+-------------------------+-----------------------------------+---------------------------------+
    | Benchmark                                   | perfmetrics-master-py27 | perfmetrics-master-samples-py37-2 | perfmetrics-master-pypy27       |
    +=============================================+=========================+===================================+=================================+
    | bench_a_call_func_without_metric            | 68.7 ns                 | 76.9 ns: 1.12x slower (+12%)      | 1.68 ns: 40.79x faster (-98%)   |
    +---------------------------------------------+-------------------------+-----------------------------------+---------------------------------+
    | bench_a_call_method_without_metric          | 75.3 ns                 | 79.6 ns: 1.06x slower (+6%)       | 1.67 ns: 45.10x faster (-98%)   |
    +---------------------------------------------+-------------------------+-----------------------------------+---------------------------------+
    | bench_call_func_with_metric                 | 447 ns                  | 500 ns: 1.12x slower (+12%)       | 1.70 ns: 262.35x faster (-100%) |
    +---------------------------------------------+-------------------------+-----------------------------------+---------------------------------+
    | bench_call_func_with_null_client            | 1.51 us                 | 1.76 us: 1.16x slower (+16%)      | 328 ns: 4.62x faster (-78%)     |
    +---------------------------------------------+-------------------------+-----------------------------------+---------------------------------+
    | bench_call_func_with_udp_client             | 17.6 us                 | 10.0 us: 1.76x faster (-43%)      | 9.39 us: 1.88x faster (-47%)    |
    +---------------------------------------------+-------------------------+-----------------------------------+---------------------------------+
    | bench_call_method_with_metric               | 483 ns                  | 519 ns: 1.08x slower (+8%)        | 1.70 ns: 283.22x faster (-100%) |
    +---------------------------------------------+-------------------------+-----------------------------------+---------------------------------+
    | bench_call_sampled_1_func_with_null_client  | 445 ns                  | 507 ns: 1.14x slower (+14%)       | 47.7 ns: 9.33x faster (-89%)    |
    +---------------------------------------------+-------------------------+-----------------------------------+---------------------------------+
    | bench_call_sampled_99_func_with_null_client | 1.59 us                 | 1.85 us: 1.16x slower (+16%)      | 339 ns: 4.70x faster (-79%)     |
    +---------------------------------------------+-------------------------+-----------------------------------+---------------------------------+

--- src/perfmetrics/tests/bench_metric.py
+++ src/perfmetrics/tests/bench_metric.py
@@ -16,7 +16,8 @@ from perfmetrics import set_statsd_client
 from perfmetrics import Metric
 from perfmetrics.statsd import null_client
 
-metricsampled = Metric(rate=0.1)
+metricsampled_1 = Metric(rate=0.1)
+metricsampled_9 = Metric(rate=0.999)
 
 INNER_LOOPS = 1000
 
@@ -24,8 +25,12 @@ INNER_LOOPS = 1000
 def func_with_metric():
     pass
 
-@metricsampled
-def func_with_metricsampled():
+@metricsampled_1
+def func_with_metricsampled_1():
+    pass
+
+@metricsampled_9
+def func_with_metricsampled_99():
     pass
 
 def func_without_metric():
@@ -51,18 +56,22 @@ def _bench_call_func(loops, f):
 
 ##
 # These four measure the overhead when there is no client installed
+# First two are baselines.
 ##
+
+def bench_a_call_func_without_metric(loops):
+    return _bench_call_func(loops, func_without_metric)
+
+def bench_a_call_method_without_metric(loops):
+    return _bench_call_func(loops, AClass().method_without_metric)
+
+
 def bench_call_func_with_metric(loops):
     return _bench_call_func(loops, func_with_metric)
-
-def bench_call_func_without_metric(loops):
-    return _bench_call_func(loops, func_without_metric)
 
 def bench_call_method_with_metric(loops):
     return _bench_call_func(loops, AClass().method_with_metric)
 
-def bench_call_method_without_metric(loops):
-    return _bench_call_func(loops, AClass().method_without_metric)
 
 ##
 # This measures the overhead of a trivial client
@@ -84,8 +93,12 @@ def bench_call_func_with_null_client(loops):
 # It also uses a trivial client so we're not
 # dependent on the order of tests.
 ##
-def bench_call_sampled_func_with_null_client(loops):
-    return _bench_call_func_with_client(loops, func_with_metricsampled, null_client)
+def bench_call_sampled_1_func_with_null_client(loops):
+    return _bench_call_func_with_client(loops, func_with_metricsampled_1, null_client)
+
+def bench_call_sampled_99_func_with_null_client(loops):
+    return _bench_call_func_with_client(loops, func_with_metricsampled_99, null_client)
+
 
 ##
 # This measures actually sending the UDP packet
