a177839170b41e40da8b7e631c9818d827ea3ec1
Jason Madden <jamadden@gmail.com>
2023-06-22 13:20:12 -0500

    Cannot get persistent to build on PyPy 3.9

--- setup.py
+++ setup.py
@@ -19,7 +19,8 @@ tests_require = [
     'zope.testrunner',
     # nti.testing > ZODB > persistent -> cffi
     # But cffi won't build on apple silicon on 3.12.
-    'nti.testing; (python_version != "3.12" and platform_machine != "arm64" and platform_system != "Darwin")',
+    # Can't get persistent to build on PyPy 3.9 either.
+    'nti.testing; (python_version != "3.12" and platform_machine != "arm64" and platform_system != "Darwin") and platform_python_implementation != "PyPy"',
     # transitive dep of nti.testing, which we don't always have, but need
     # for our emulation
     'zope.schema',
